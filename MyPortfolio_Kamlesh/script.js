9a73bac65ded088d1d68d890d1d229b074d7c153
Kamlesh Kumar Maurya <53991283+KamleshMaurya@users.noreply.github.com>
2022-07-14 13:07:11 +0530

    Update script.js

--- MyPortfolio_Kamlesh/script.js
+++ MyPortfolio_Kamlesh/script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software developer", "Programming"],
+        strings: ["Software developer", "Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
